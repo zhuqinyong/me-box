061c5d7cef462e4c7d34b310748161bf07519f7c
zhuqinyong <18326657779@163.com>
2022-02-23 19:24:37 +0800

    启动类

--- src/me/tsinyong/box/Launcher.java
+++ src/me/tsinyong/box/Launcher.java
@@ -1,7 +1,12 @@
 package me.tsinyong.box;
 
+/**
+ * All start here
+ */
 public class Launcher {
     public static void main(String[] args) {
+        System.out.println("start here");
 
+        System.out.println("stop here");
     }
 }
